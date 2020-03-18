3811bf483e7d6fe7eb885ab4d37f17898766d048
kmbarnett <kmbarnett94@gmail.com>
2020-03-18 12:40:53 -0600

    Add structure for app and several functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import noImage from '../../assets/image-not-found.png';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 import SearchForm from './../SearchForm/SearchForm.js';
 import Menu from './../Menu/Menu.js';
 import NewsContainer from './../NewsContainer/NewsContainer.js';
 
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local,
+      entertainment,
+      health,
+      science,
+      technology,
+      currentArticles: 'local'
     }
   }
+
+  handleError(e) {
+    e.target.src = noImage
+  }
+
+  changeCurrentArticles = (articles) => {
+    this.setState({currentArticles: articles})
+  }
+
 
   render () {
     return (
@@ -19,11 +39,11 @@ class App extends Component {
         <header>
           <SearchForm />
         </header>
-        <nav>
-          <Menu />
-        </nav>
         <main>
-          <NewsContainer />
+          <nav>
+          <Menu changeCurrentArticles = {this.changeCurrentArticles}/>
+          </nav>
+          <NewsContainer  articles={this.state[this.state.currentArticles]} handleError={this.handleError}/>
         </main>
       </section>
     );
