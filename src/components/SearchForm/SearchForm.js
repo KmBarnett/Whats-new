306edd82aeacf27a2fe28751a968ed0819c6aa3c
kmbarnett <kmbarnett94@gmail.com>
2020-03-18 17:35:47 -0600

    Add search call

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,8 +5,18 @@ const SearchForm = (props) => {
     return (
       <form>
         <h3 className='unselectable name'>What's <span>New?</span></h3>
-        <input on className='search-input' type='text' placeholder='Search News Articles Here'/>
-        <button onClick={props.searchArticles} className='unselectable search-btn' type='button'>Search Now</button>
+        <input id='search'  onKeyDown={(e) =>  {
+          console.log(e.keyCode);
+          if (e.key === 'Enter') {
+            e.preventDefault()
+          }
+          (e.keyCode === 13) && props.searchArticles(e.target.value)
+        } } className='search-input' type='text' placeholder='Search News Articles Here'/>
+        <button type='button' onClick={(e) => {
+            e.preventDefault()
+            let input = document.getElementById('search').value
+            props.searchArticles(input)
+          }} className='unselectable search-btn' type='button'>Search Now</button>
       </form>
     )
 }
