2a00dc72c140b045900f57c94a5e1029904e8fd8
kmbarnett <kmbarnett94@gmail.com>
2020-03-18 12:50:46 -0600

    Fix change to functional component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,25 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './SearchForm.css';
 
-class SearchForm extends Component {
-  constructor(props) {
-    super()
-    this.props = props;
-    this.state = {
-
-    }
-  }
-
-  render() {
+const SearchForm = (props) => {
     return (
       <form>
         <h3 className='unselectable name'>What's <span>New?</span></h3>
-        <input className='search-input' type='text' placeholder='Search News Articles Here'/>
-        <button className='unselectable search-btn' type='button'>Search Now</button>
+        <input on className='search-input' type='text' placeholder='Search News Articles Here'/>
+        <button onClick={props.searchArticles} className='unselectable search-btn' type='button'>Search Now</button>
       </form>
     )
-  }
-
 }
 
 export default SearchForm;
